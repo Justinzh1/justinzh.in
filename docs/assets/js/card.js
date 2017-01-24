022569ddd92c538780a5bdf72833206cfa196278
justinzh1 <justinzhongvs@gmail.com>
2017-01-24 14:30:43 -0800

    updated favicon, updated description of Hermione, updated resume

--- docs/assets/js/card.js
+++ docs/assets/js/card.js
@@ -1,25 +1,4 @@
 $(document).ready(function() {
-    // $('#hermione').click(function(){
-    //     console.log('hello');
-    //     $('#hermione-info').css('display','block');
-    //     $('#hermione-info').addClass('active-card');
-    //     $('.nav').css('background-color', 'black');
-    //     $('body').css('overflow', 'hidden');
-    //     $('#justin').css('color', 'white');
-    // });
-    // $('.active-card').click(function(){
-    //     $('#hermione-info').css('display', 'none');
-    //     $('#herminoe-info').removeClass('active-card');
-    // });
-    //
-    // $('#cpu').click(function(){
-    //     console.log('hello');
-    //     $('#hermione-info').css('display','block');
-    //     $('#hermione-info').addClass('active-card');
-    //     $('.nav').css('background-color', 'black');
-    //     $('body').css('overflow', 'hidden');
-    //     $('#justin').css('color', 'white');
-    // });
 
     $('.toolbar').mouseenter(function() {
         // $(this).css('color', 'white');
@@ -34,17 +13,10 @@ $(document).ready(function() {
         id = $(this).attr('id');
         tab = '#' + id;
         info = tab + '-info';
-        // $(tab).click(function() {
-            console.log('working ' + this);
-            $(info).css('display', 'block');
-            $(info).addClass('active-card');
-            $('.nav').css('background-color', 'black');
-            $('body').css('overflow', 'hidden');
-            $('#justin').css('color', 'white');
-        // });
-        // $('.active-card').click(function() {
-        //     $(info).css('display', 'none');
-        //     $(info).removeClass('active-card');
-        // });
+        $(info).css('display', 'block');
+        $(info).addClass('active-card');
+        $('.nav').css('background-color', 'black');
+        $('body').css('overflow', 'hidden');
+        $('#justin').css('color', 'white');
     });
 });
